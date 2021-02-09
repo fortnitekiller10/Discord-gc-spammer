b8c3e4b9ded23efef6ae7e1174ade6637670212e
KAMKAZEMARCI <74118308+KAMKAZEMARCI@users.noreply.github.com>
2021-02-09 23:11:13 +0100

    Add files via upload

---
+++ gc spammer.py
@@ -0,0 +1,17 @@
+import json, requests
+
+with open('config.json') as f:
+    config = json.load(f)
+
+token = config.get('token')
+
+
+while True:
+    global cycling
+    cycling = True
+    while cycling:
+      headers = {
+          'Authorization': token,
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.308 Chrome/78.0.3904.130 Electron/7.3.2 Safari/537.36'
+      }
+      requests.post('https://discordapp.com/api/v8/users/@me/channels', headers=headers, json={"recipients":["useridhere","useridhere"]})
