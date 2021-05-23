8abff44215463aca585fb8a408174edda6110028
SKYNETMARCI <74118308+KAMKAZEMARCI@users.noreply.github.com>
2021-05-23 18:08:23 +0200

    Update gc spammer.py

--- gc spammer.py
+++ gc spammer.py
@@ -14,4 +14,4 @@ while True:
           'Authorization': token,
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.308 Chrome/78.0.3904.130 Electron/7.3.2 Safari/537.36'
       }
-      requests.post('https://discordapp.com/api/v8/users/@me/channels', headers=headers, json={"recipients":["useridhere","useridhere"]})
+      requests.post('https://discordapp.com/api/v9/users/@me/channels', headers=headers, json={"recipients":["useridhere","useridhere"]})
